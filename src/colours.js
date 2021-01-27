a0f925df7e84eaf76b95fd42852774ce54113fa2
Lauren Armstrong <lauren.armstrong@prudential.co.uk>
2021-01-27 17:13:26 +0000

    Adding navbar component

---
+++ src/colours.js
@@ -0,0 +1,5 @@
+export const white = '';
+export const black = '';
+export const grey = '';
+export const purple = '#401F34';
+export const blue = '#25B8C6';
---
+++ src/pages/AboutUs.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import NavBar from '../pages/NavBar';
+
+const AboutUs = () => {
+    return (
+        <NavBar />
+    )
+}
+export default AboutUs;
