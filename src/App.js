e0e87e2f1c95e697ba0805191028af8b2342d5bc
Kieren Kershaw <insanist38@gmail.com>
2021-01-28 20:32:17 +0000

    Rows and Cols for Homepage

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import Home from './pages/Home';
 import {Router, Switch, Route} from 'react-router-dom'
 import AboutUs from './pages/AboutUs';
 
+import NavBar from './components/NavBar'
+
 function App() {
   return (
       <div className="AppMain">
+        <NavBar />
         <Switch>
           <Route exact path="/" component={Home}/>
           <Route path="/about" component={AboutUs}/>
