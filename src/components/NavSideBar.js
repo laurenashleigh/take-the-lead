454f8355f516e602d4ef2d00c3d1c205fdb7594f
Lauren Armstrong <lauren.armstrong@prudential.co.uk>
2021-01-28 19:51:39 +0000

    side nav styling

--- src/components/NavSideBar.js
+++ src/components/NavSideBar.js
@@ -3,13 +3,20 @@ import { Nav } from 'react-bootstrap';
 
 const NavSideBar = () => {
     return (
-        <Nav defaultActiveKey="/home" className="flex-column">
-            <Nav.Link href="/home">Waltz</Nav.Link>
-            <Nav.Link href="">Jive</Nav.Link>
-            <Nav.Link href="">Quick Step</Nav.Link>
-            <Nav.Link href="">Cha Cha</Nav.Link>
-            <Nav.Link href="">Fox Trot</Nav.Link>
-        </Nav>
+        <div className="side-nav">
+            <Nav defaultActiveKey="/home" className="flex-column">
+                <Nav.Link href="/home">Waltz</Nav.Link>
+                <hr />
+                <Nav.Link href="">Jive</Nav.Link>
+                <hr />
+                <Nav.Link href="">Quick Step</Nav.Link>
+                <hr />
+                <Nav.Link href="">Cha Cha</Nav.Link>
+                <hr />
+                <Nav.Link href="">Fox Trot</Nav.Link>
+            </Nav>
+        </div>
+        
     )
 }
 
