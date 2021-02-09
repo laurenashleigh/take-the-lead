1146e032bd692d7f17d55359908ca85f353f64a9
Lauren Armstrong <lauren.armstrong@prudential.co.uk>
2021-02-09 17:14:05 +0000

    contact us component

---
+++ src/components/ContactUs.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Jumbotron, Button} from 'react-bootstrap';
+
+const ContactUs = () => {
+    return (
+         <Jumbotron>
+            <h1>Get in Touch!</h1>
+            <p>
+            For enquiries or bookings please contact Val on: 07789 957 071 or email info@taketheleaddance.co.uk
+            </p>
+            <p>
+                <Button variant="primary">Learn more</Button>
+            </p>
+        </Jumbotron>
+    )
+   
+}
+
+export default ContactUs;
